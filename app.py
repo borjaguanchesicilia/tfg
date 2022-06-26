dd58ea4ec9707cfb43b9a1583bc7201037dab330
Maquina Virtual Linux <bg.sicilia@gmail.com>
2022-06-26 04:49:45 -0700

    Prueba test 7

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import tkinter
 from src.controlador.c_general import ControladorGeneral
 from src.librerias import *
 from src.vista.v_general import Vista
-
+tkinter.use('Agg')
 
 class Aplicacion(Tk):
     def __init__(self):
