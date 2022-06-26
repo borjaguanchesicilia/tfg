d6f81e12c049a31cc33305bd2da76cca232ca13a
Maquina Virtual Linux <bg.sicilia@gmail.com>
2022-06-26 09:15:26 -0700

    Workflow configuracion

---
+++ script.py
@@ -0,0 +1,8 @@
+import os
+
+badget = 'coverage.svg'
+
+os.system('python3.6 -m coverage run -m pytest ./pruebas/controlador/t_c_parametros.py')
+
+os.system(f'rm {badget}')
+os.system('coverage-badge -o coverage.svg')
