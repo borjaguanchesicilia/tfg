a1cb5aa4a01a0f477dc5db3731d9ba8c5b3ac7bd
Maquina Virtual Linux <bg.sicilia@gmail.com>
2022-07-03 13:22:31 -0700

    Test Funciones Aux

---
+++ pruebas/t_funciones_aux.py
@@ -0,0 +1,8 @@
+from src.funciones_aux import conversor_aeropueros, obtener_nombre
+
+
+def test_obtener_nombre():
+    assert obtener_nombre("./ficheros/aviones.csv") == "aviones.csv"
+
+def test_conversor_aeropueros():
+    assert conversor_aeropueros(['Tenerife Norte', 'Tenerife Sur']) == ['TFN', 'TFS']
--- script.py
+++ script.py
@@ -14,5 +14,9 @@ os.system(
     "python3.6 -m coverage run -m pytest ./pruebas/modelo/t_lectura_fichero.py"
 )
 
+os.system(
+    "python3.6 -m coverage run -m pytest ./pruebas/t_funciones_aux.py"
+)
+
 os.system(f"rm {badget}")
 os.system("coverage-badge -o coverage.svg")
