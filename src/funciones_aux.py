30c7d3fc25e6fc19df807311ecfdff29a9777da7
Maquina Virtual Linux <bg.sicilia@gmail.com>
2022-07-05 06:36:11 -0700

    Comentarios Funciones aux

--- src/funciones_aux.py
+++ src/funciones_aux.py
@@ -1,4 +1,18 @@
 def obtener_nombre(ruta):
+    """
+    Se realiza la extracción del nombre de un fichero dada la ruta donde se
+    encuentra.
+
+    Parameters
+    ----------
+    ruta : str
+        Cadena de texto que contiene la ruta dónde se encuentra el fichero
+
+    Returns
+    -------
+    str
+        Cadena de texto que contiene el nombre del fichero.
+    """
 
     nombre_fichero = ruta[::-1]
 
@@ -12,6 +26,21 @@ def obtener_nombre(ruta):
 
 
 def conversor_aeropueros(lista_original):
+    """
+    Se realiza la conversión de los nombres de los aeropuertos en sus
+    correspondientes códigos de la IATA.
+
+    Parameters
+    ----------
+    lista_original : list
+        Lista que contiene nombres de aeropuertos
+
+    Returns
+    -------
+    list
+        Lista que contiene los aeropuertos convertidos a código IATA.
+    """
+
     conversor_aeropuertos = {
         "Arrecife": "ACE",
         "Fuerteventura": "FUE",
